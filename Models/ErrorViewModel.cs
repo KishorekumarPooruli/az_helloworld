eeff8fc959585ff84013eb2ed761eeae651f6013
KishorekumarPooruli <kishorekumar.pooruli@outlook.com>
2025-06-25 10:26:27 +0100

    push code to azure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace az_helloworld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
